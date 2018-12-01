97756383a6e1dd50eb40fe7c9bc94e234a6e3765
rabarbar362 <barbara.nogas@gmail.com>
2018-12-01 12:51:50 +0100

    Main class code refactor

--- src/main/java/com/rabarbar362/terminalweather/Main.java
+++ src/main/java/com/rabarbar362/terminalweather/Main.java
@@ -4,39 +4,44 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String args[]) throws IOException {
+    private static void currentWeather(WeatherData weatherData) {
+        CurrentWeather currentWeather = new CurrentWeather(weatherData.getCurrentWeather());
+        currentWeather.printCurrentWeather();
+    }
+
+    private static void forecastWeather(WeatherData weatherData) {
+        ForecastWeather forecastWeather = new ForecastWeather(weatherData.getForecastWeather());
+        forecastWeather.printForecast();
+    }
+
+    public static void main(String[] args) throws IOException {
 
         int numberOfDays;
 
         if (args.length > 0) {
             try {
                 numberOfDays = Integer.parseInt(args[0]);
+                UserIPAddress IPAddress = new UserIPAddress();
+                System.out.println("Your IP address is: " + IPAddress.getIPAddress());
+                UserLocation userLocation = new UserLocation(IPAddress.getIPAddress());
 
-                if (numberOfDays >7) {
+                if (numberOfDays > 7) {
+                    WeatherData data = new WeatherData(userLocation.getLatitude(), userLocation.getLongitude(),numberOfDays);
+                    currentWeather(data);
                     System.out.println("--------- The forecast is available only for 7 days ahead! --------- \n");
+                    forecastWeather(data);
+                } else if (numberOfDays == 0) {
+                    WeatherData data = new WeatherData(userLocation.getLatitude(), userLocation.getLongitude(),numberOfDays);
+                    currentWeather(data);
+                } else {
+                    WeatherData data = new WeatherData(userLocation.getLatitude(), userLocation.getLongitude(),numberOfDays);
+                    currentWeather(data);
+                    forecastWeather(data);
                 }
-
-                    UserIPAddress IPAddress = new UserIPAddress();
-                    System.out.println("Your IP address is: " + IPAddress.getIPAddress());
-                    UserLocation userLocation = new UserLocation(IPAddress.getIPAddress());
-                    WeatherData weatherData = new WeatherData(userLocation.getLatitude(), userLocation.getLongitude(), numberOfDays);
-                    CurrentWeather currentWeather = new CurrentWeather(weatherData.getCurrentWeather());
-                    currentWeather.printCurrentWeather();
-                    ForecastWeather forecastWeather = new ForecastWeather(weatherData.getForecastWeather());
-                    forecastWeather.printForecast();
-
 
             } catch (NumberFormatException e) {
                 System.out.println("Invalid arguments.");
             }
-        } else {
-            numberOfDays = 0;
-            UserIPAddress IPAddress = new UserIPAddress();
-            System.out.println("Your IP address is: " + IPAddress.getIPAddress());
-            UserLocation userLocation = new UserLocation(IPAddress.getIPAddress());
-            WeatherData weatherData = new WeatherData(userLocation.getLatitude(), userLocation.getLongitude(), numberOfDays);
-            CurrentWeather currentWeather = new CurrentWeather(weatherData.getCurrentWeather());
-            currentWeather.printCurrentWeather();
         }
     }
 }
